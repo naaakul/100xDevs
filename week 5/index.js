79a0d4b4edb4e9b6eb0f24f85c227c6ac6449409
Nakul Chouksey <71.nakul@gmail.com>
2024-10-12 00:37:30 +0530

    week 5

--- week 5/index.js
+++ week 5/index.js
@@ -2,17 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.get("/sum", (req, res) => {
-    const a = parseInt(req.query.a);
-    const b = parseInt(req.query.b);
+// app.get("/sum", (req, res) => {
+//     const a = parseInt(req.query.a);
+//     const b = parseInt(req.query.b);
+
+//     res.json({
+//         answer : a + b
+//     })
+// })
+// http://localhost:3000/sum?a=4&b=5
+
+app.get("/sum/:a/:b", (req, res) => {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
 
     res.json({
         answer : a + b
     })
 })
-
-// app.get("/multiply", (req, res) => {
-    
-// })
+// http://localhost:3000/sum/5/6
 
 app.listen(3000);
